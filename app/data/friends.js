ca8a7a09137f42e0924080324c117050f17da890
Marsha Miquela <marshamiquela@Virgilios-MBP.home>
2018-02-11 01:00:08 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+  {
+    name: 'Molokai',
+    photo: 'https://www.gohawaii.com/islands/molokai',
+    scores: [
+      '1',
+      '5',
+      '5',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Canyonlands',
+    photo: 'https://www.nationalparks.org/explore-parks/canyonlands-national-park',
+    scores: [
+      '2',
+      '4',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '2',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Plitvice',
+    photo: 'https://en.wikipedia.org/wiki/Plitvice_Lakes_National_Park',
+    scores: [
+      '4',
+      '3',
+      '5',
+      '1',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1'
+    ]
+  },
+  {
+    name: 'Chocolate Hills',
+    photo: 'https://en.wikipedia.org/wiki/Chocolate_Hills',
+    scores: [
+      '5',
+      '1',
+      '2',
+      '1',
+      '3',
+      '4',
+      '1',
+      '3',
+      '3',
+      '4'
+    ]
+  },
+  {
+    name: 'Cloe',
+    photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '1'
+    ]
+  },
+  {
+    name: 'Mrs. Piggy',
+    photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '1',
+      '3',
+      '2',
+      '5',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Beyonce Knowles',
+    photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+    scores: [
+      '5',
+      '4',
+      '4',
+      '2',
+      '4',
+      '4',
+      '3',
+      '2',
+      '3',
+      '3'
+    ]
+  },
+  {
+    name: 'Adele Adkins',
+    photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+    scores: [
+      '5',
+      '2',
+      '3',
+      '1',
+      '5',
+      '1',
+      '3',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Kermit the Frog',
+    photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+    scores: [
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5'
+    ]
+  },
+  {
+    name: 'Elmo',
+    photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+    scores: [
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1'
+    ]
+  }
+];
+
+module.exports = friendsArray;
